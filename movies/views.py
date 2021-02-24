e7587456322b7269dedbf7d6a049ef0618834065
brahim024 <ifninos168@gmail.com>
2021-02-24 15:55:48 +0100

    add views to render data

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .forms import MovieForm
+from models import Movie
 # Create your views here.
+
+def home(request):
+    form=MovieForm(request.POST or None)
+    if request.method='POST':
+        if form.is_vald():
+            form.save()
+    form=MovieForm()
+    qs=Movie.objects.all()
+
+    context={
+        'form':form,
+        'qs':qs
+    }
+
+    return render(request,'home.html',context)
